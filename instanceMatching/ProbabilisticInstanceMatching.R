3c08934d1dc678596374ace7efd588167f3ba8f9
Chris Davis <cbdavis@cbdavis-desktop.(none)>
2012-05-25 11:54:57 +0200

    updating code

--- instanceMatching/ProbabilisticInstanceMatching.R
+++ instanceMatching/ProbabilisticInstanceMatching.R
@@ -1,6 +1,7 @@
 library(reshape) #colsplit
 library(gdata) #trim
 library(geosphere)
+library(RecordLinkage) #string matching
 
 #TODO what is this again?
 getScore <- function(charVector, countsVector, numEntries){
@@ -113,6 +114,154 @@ getTokenLookupMap <- function(charVector){
   return(tokenLookup)
 }
 
+
+lookForMatches <- function(df1, df2, #the two data frames to compare
+                           soupColumnsFromDataSet1, #list of columns to combine together for matching
+                           soupColumnsFromDataSet2, 
+                           lat1=NULL, #coordinate columns in first data frame
+                           lon1=NULL, 
+                           lat2=NULL, #coordinate columns in second data frame 
+                           lon2=NULL, 
+                           outputFilename="test.csv", #file that shows possible matching entries side-by-side from both data sets
+                           maxDistance=20000){ #only match if within this distance of each other
+
+  df1$soup = apply(df1[,soupColumnsFromDataSet1], 1, function(x) paste(x, sep="", collapse=" "))
+  df1$soup = removeTheWeirdness(df1$soup)
+  
+  df2$soup = apply(df2[,soupColumnsFromDataSet2], 1, function(x) paste(x, sep="", collapse=" "))
+  df2$soup = removeTheWeirdness(df2$soup)
+
+  #now do something intelligent... um
+  #remove weirdness from the soup
+  #enipediaData$soup = removeTheWeirdness(enipediaData$soup)
+  #data$soup = removeTheWeirdness(data$soup)
+  
+  df1TokenLookup = getTokenLookupMap(df1$soup)
+  df2TokenLookup = getTokenLookupMap(df2$soup)
+  #eniTokenLookup = getTokenLookupMap(enipediaData$soup)
+  #extDataTokenLookup = getTokenLookupMap(data$soup)
+  
+  #contains indices for matches
+  possibleMatches = NULL
+  
+  #compares possible matches with info from both data sources
+  matches = NULL
+  
+  #iterate over each set of terms in the soup describing each power plant
+  index = 1
+  for (df1SoupEntry in df1$soup){
+    #convert to tokens
+    #extSoup = removeTheWeirdness(extSoup)
+    df1DataTokens = unique(unlist(strsplit(df1SoupEntry, split=" ")))
+    #extDataTokens = unique(unlist(strsplit(extSoup, split=" ")))
+    
+    #this is created for every entry in the external data set
+    matchesLookup = data.frame(informationEntropy = rep(0, dim(df2)[1]),
+                               distance = rep(0, dim(df2)[1]))
+    
+    print("=====================================")
+    for (token in df1DataTokens){
+      informationEntropy = -log10(length(df1TokenLookup[[token]])/dim(df1)[1])
+      #only match based on high entropy tokens - this avoids generic terms
+      #find the entries that have the most number of matches
+      #if (is.finite(informationEntropy)){
+      if (informationEntropy > 1 && is.finite(informationEntropy)){
+        #see if this token also has high information entropy in the other dataset
+        informationEntropyInOtherDataset = -log10(length(df2TokenLookup[[token]])/dim(df2)[1])
+        if (informationEntropyInOtherDataset > 1 && is.finite(informationEntropyInOtherDataset)){
+          #if (is.finite(informationEntropyInOtherDataset)){
+          print(token)
+          df2Matches = unique(df2TokenLookup[[token]])
+          matchesLookup$informationEntropy[df2Matches] = matchesLookup$informationEntropy[df2Matches] + informationEntropyInOtherDataset
+          }
+        }
+      }
+    
+      #ok, who has information entropy above zero? (who has the most unique matching terms?)
+      locs = which(matchesLookup > 0)
+      if(length(locs) > 0){
+        #check if coordinate fields are set
+        if (!is.null(lat1) && !is.null(lon1) && !is.null(lat2) && !is.null(lon2)){
+          #find the distances between possible matches
+          
+          distances = distCosine(cbind(df1[index, lon1], 
+                                       df1[index, lat1]), 
+                                 cbind(df2[locs, lon2],
+                                       df2[locs, lat2])
+                                 )
+          matchesLookup$distance[locs] = distances
+        }
+      }
+    
+      #don't consider these entries
+      locs = which(matchesLookup$informationEntropy == 0)
+      matchesLookup$distance[locs] = NA
+      matchesLookup$informationEntropy[locs] = NA
+      
+      #now put possible matches together into a spreadsheet format where we can verify if everything looks ok
+      #TODO parameterize this, make a function out of this whole code
+      #find all possible matches within a 20 km radius
+      #TODO should just write out entries here for comparison
+      locs = which(matchesLookup$distance < 20000)
+      if(length(locs) > 0){
+        jaccardIndexValues = unlist(lapply(df2$soup[locs], function(x) {jaccard_index(x,df1$soup[index])}))
+        possibleMatches = rbind(possibleMatches, cbind(index, locs, jaccardIndexValues, matchesLookup$distance[locs]))
+      }
+      index = index + 1
+    }
+
+  #write out the Jaccard Index as well
+  
+  ldiff = levenshteinSim(df1$soup[possibleMatches[,1]], 
+                         df2$soup[possibleMatches[,2]])
+  
+  jdiff = jarowinkler(df1$soup[possibleMatches[,1]], 
+                      df2$soup[possibleMatches[,2]], 
+                      r=0.5)
+  
+  matches = cbind(df1[possibleMatches[,1],soupColumnsFromDataSet1], 
+                  df2[possibleMatches[,2],soupColumnsFromDataSet2],
+                  possibleMatches[,3], #jaccard index values
+                  possibleMatches[,4], #distance
+                  ldiff, #levenshtein distance
+                  jdiff) #jaro winkler distance
+  
+  nCol = length(colnames(matches))
+  colnames(matches)[nCol-3] = 'jaccard index'
+  colnames(matches)[nCol-2] = 'geo distance'
+  colnames(matches)[nCol-1] = 'levenshtein'
+  colnames(matches)[nCol] = 'jaro winkler'
+  
+  #write results to a file where we can compare the data and do a sanity check
+  write.csv(matches, file=outputFilename, row.names=FALSE)
+}
+
+#http://en.wikipedia.org/wiki/Jaccard_index
+#Look at length of intersection and union of tokens in two strings to determine similarity
+jaccard_index <- function(text1, text2){
+  set1 = tokenize(text1)
+  set2 = tokenize(text2)
+  jaccard_index = length(intersect(set1, set2)) / length(union(set1, set2))
+  return(jaccard_index)
+}
+
+#return a set of tokens contained within a string
+tokenize <- function(text){
+  tokenList = unique(unlist(strsplit(text, split=" ")))
+  return(tokenList)
+}
+
+#could have a matrix of the scores for all possible matches
+match_based_on_jaccard_index <- function(df1, df2){
+  for (index in c(1:dim(df2)[1])){
+    print(index)
+    jaccard_index_values = unlist(lapply(df1$soup, function(x) {jaccard_index(x,df2$soup[index])}))
+    maxLoc = which.max(jaccard_index_values)
+    print(paste(df1$name[maxLoc], df2$name[index]))
+  }
+}
+
+
 #never ever ever convert strings to factors
 options(stringsAsFactors = FALSE)
 
@@ -121,7 +270,7 @@ library(SPARQL)
 setwd("/home/cbdavis/Desktop/svn/ChrisDavis/PhD/Enipedia/InstanceMatching/")
 
 data = read.csv("/home/cbdavis/Downloads/power_plants_from_industryabout.csv", header=TRUE, sep=",")
-data = data[-which(data$country != "France"),]
+data = data[-which(data$country != "Netherlands"),]
 #make a soup column for matching - this contains everything that could be used to create the name of the power plant
 #TODO need to have a synonym or relationship lookup - need to add more information, give more context to help with matching
 #such as SNET is now E.ON France
@@ -137,7 +286,7 @@ queryString = paste(getPrefixes(),
                     ?x rdf:type cat:Powerplant .
                     ?x prop:City ?city . 
                     ?x rdfs:label ?name . 
-                    ?x prop:Country a:France . 
+                    ?x prop:Country a:Netherlands . 
                     ?x prop:City ?city .
                     ?x prop:State ?state .
                     ?x prop:Ownercompany ?owner . 
@@ -148,86 +297,16 @@ enipediaData = d$results
 coords = extractCoordinates(enipediaData$point)
 enipediaData$lat = coords$lat
 enipediaData$lon = coords$lon
-enipediaData$soup = paste(enipediaData$name, enipediaData$owner, enipediaData$city, enipediaData$state)
 
-enipediaCounts = getFrequencyListOfWords(enipediaData$soup)
+lookForMatches(df1 = data, 
+               df2 = enipediaData, 
+               soupColumnsFromDataSet1 = c('name', 'owner', 'area', 'other_name', 'owners'), 
+               soupColumnsFromDataSet2 = c('name', 'owner', 'city', 'state'), 
+               lat1 = 'latitude',
+               lon1 = 'longitude',
+               lat2 = 'lat',
+               lon2 = 'lon',
+               maxDistance=20000)
 
-#now do something intelligent... um
-#remove weirdness from the soup
-enipediaData$soup = removeTheWeirdness(enipediaData$soup)
-data$soup = removeTheWeirdness(data$soup)
-
-eniTokenLookup = getTokenLookupMap(enipediaData$soup)
-extDataTokenLookup = getTokenLookupMap(data$soup)
-
-#iterate over each set of terms in the soup describing each power plant
-index = 1
-for (extSoup in data$soup){
-  #convert to tokens
-  extSoup = removeTheWeirdness(extSoup)
-  extDataTokens = unique(unlist(strsplit(extSoup, split=" ")))
-
-  matchesLookup = data.frame(informationEntropy = rep(0, length(enipediaData$name)),
-                             distance = rep(0, length(enipediaData$name)))
-  
-  print("=====================================")
-  for (token in extDataTokens){
-    informationEntropy = -log10(length(extDataTokenLookup[[token]])/length(data$name))
-    #only match based on high entropy tokens - this avoids generic terms
-    #find the entries that have the most number of matches
-    #if (is.finite(informationEntropy)){
-    if (informationEntropy > 1 && is.finite(informationEntropy)){
-      #see if this token also has high information entropy in the other dataset
-      informationEntropyInOtherDataset = -log10(length(eniTokenLookup[[token]])/length(enipediaData$name))
-      if (informationEntropyInOtherDataset > 1 && is.finite(informationEntropyInOtherDataset)){
-      #if (is.finite(informationEntropyInOtherDataset)){
-        print(token)
-        #need to do something intelligent here that rates the suitability of the matches
-        #could just do string comparisons here
-        #print(eniTokenLookup[token])
-        
-        #print(informationEntropyInOtherDataset)
-        #print(informationEntropy)
-        #look at all the matches within a certain radius, and the number of tokens matched
-        #need data on the number of matching tokens per possible candidate match
-        #collect data on information entry of term, number of matching terms per candidate, and distance of candidates
-        eniMatches = unique(eniTokenLookup[[token]])
-        
-        matchesLookup$informationEntropy[eniMatches] = matchesLookup$informationEntropy[eniMatches] + informationEntropyInOtherDataset
-        
-        #look at information entropy of coordinates - information entropy of continious variables
-        #look at all things within x km of the point?  or of everything in the selection?
-        
-      }
-    }
-  }
-  
-  #ok, who has information entropy above zero? (who has the most unique matching terms?)
-  locs = which(matchesLookup > 0)
-  if(length(locs) > 0){
-    #find the distances to each of these
-    
-    distances = distCosine(cbind(data$longitude[index], 
-                                 data$latitude[index]), 
-                           cbind(enipediaData$lon[locs],
-                                 enipediaData$lat[locs])
-                           )
-    matchesLookup$distance[locs] = distances
-  }
-  
-  #TODO need to do emissions comparison also, look for obvious things like hydro and coal
-  #the emissions should fit within certain bins
-  
-  #don't consider these entries
-  locs = which(matchesLookup$informationEntropy == 0)
-  matchesLookup$distance[locs] = NA
-  matchesLookup$informationEntropy[locs] = NA
-  
-  #this is nice - how does it differ for all of the matches
-  #plot(matchesLookup$distance, matchesLookup$informationEntropy)
-  
-  index = index + 1
-}
-
-
-
+#calculate Jaccard Index
+#jaccardIndexValues = unlist(lapply(df1$soup, function(x) {jaccard_index(x,df2$soup[1])}))
